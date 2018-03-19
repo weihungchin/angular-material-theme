037ebd8fa6a4593a21e3604bcf709b5a9f0228a7
WeiHung <whc1g10@gmail.com>
2018-03-19 21:35:07 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from "./app.component";
 import { MatIconModule } from "@angular/material/icon";
 import { HttpClientModule } from "@angular/common/http";
 import { CustomIconService } from "./services/custom-icon.service";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatIconModule, HttpClientModule],
-  providers: [
-    CustomIconService
+  imports: [
+    BrowserModule,
+    MatIconModule,
+    HttpClientModule,
+    MatTabsModule,
+    MatToolbarModule
   ],
+  providers: [CustomIconService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
